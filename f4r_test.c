1202406f7dca327ffbbfc97e7e59675c53d1b533
Roque <roqscheer@gmail.com>
2017-01-04 17:36:55 -0200

    Modified to use C Unit framework

--- f4r_test.c
+++ f4r_test.c
@@ -23,6 +23,10 @@
 
 #include <sys/types.h>
 
+#include <CUnit/CUnit.h>
+#include <CUnit/Basic.h>
+
+
 // Test open and close
 //
 void test_open( void)
@@ -33,16 +37,21 @@ void test_open( void)
     sprintf( filename, "testfile%d", rand());
     
     fd = open( filename, O_RDWR | O_CREAT, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP);
-    if ( fd < 0 )
-        printf( "test_open: Open failed with error %d\n", errno);
-    else if ( close(fd) < 0)
-        printf( "test_open: close failed with error %d\n", errno);
-    else
-        printf( "test_open: Success\n");
+    CU_ASSERT( fd >= 0);
+
+    CU_ASSERT( close(fd) >= 0);
+    
+    // Open without O_CREAT, must exist
+    fd = open( filename, O_RDWR, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP);
+    CU_ASSERT( fd >= 0);
+
+    CU_ASSERT( close(fd) >= 0);
+    
+    CU_ASSERT( unlink( filename) == 0);
 }
 
 
-// Test open and close
+// Test file offset setting and handling
 //
 void test_offset( void)
 {
@@ -54,43 +63,205 @@ void test_offset( void)
     sprintf( filename, "testfile%d", rand());
     
     fd = open( filename, O_RDWR | O_CREAT, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP);
-    if ( fd < 0 )
-        printf( "test_offset: Open failed with error %d\n", errno);
-        
+    CU_ASSERT(  fd >= 0 );        
 
     memset( buffer1, '*', 512);
-    if ( write( fd, buffer1, 512) < 512)
-        printf( "test_offset: First write failed\n");
-        
-    if ( lseek( fd, 80, SEEK_SET) < 0 )
-        printf( "test_offset: first lseek failed\n");
+    CU_ASSERT( write( fd, buffer1, 512) == 512);        
+    CU_ASSERT( lseek( fd, 80, SEEK_SET) >= 0 );
         
     memset( buffer1 + 80, '=', 80);  // Change 80 bytes starting at offset 80
-    if ( write( fd, buffer1 + 80, 80) < 80)  // and write only bytes changed
-        printf( "test_offset: Second write failed\n");
+    CU_ASSERT( write( fd, buffer1 + 80, 80) == 80);  // and write only bytes changed
         
-    if ( lseek( fd, 0, SEEK_SET) < 0 )
-        printf( "test_offset: second lseek failed\n");
-    result = read( fd, buffer2, 512);
-    if ( result  < 512)
-        printf( "test_offset: Read failed read %d bytes\n", result);
+    CU_ASSERT( lseek( fd, 0, SEEK_SET) >= 0 );
 
-    if ( memcmp( buffer1, buffer2, 512) != 0 )
-        printf( "test_offset: Comparison failed\n");
+    // Read entire file and compare
+    result = read( fd, buffer2, 512);
+    CU_ASSERT( result == 512);
+
+    CU_ASSERT( memcmp( buffer1, buffer2, 512) == 0 );
     
-    if ( close(fd) < 0)
-        printf( "test_offset: Close failed with error %d\n", errno);
-    else
-        printf( "test_offset: Success\n");
+    CU_ASSERT( close(fd) >= 0);
+    
+    CU_ASSERT( unlink( filename) == 0);
+}
+
+// Test extending files
+//
+void test_extend( void)
+{
+    int fd;
+    char filename[ 32];
+    char buffer1[ 1024] = {0},
+         buffer2[ 1024];
+         
+    sprintf( filename, "testfile%d", rand());
+    
+    fd = open( filename, O_RDWR | O_CREAT, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP);
+    CU_ASSERT( fd >= 0 );
+    
+    memset( buffer1, '*', 512);
+    CU_ASSERT( write( fd, buffer1, 512) == 512);
+    
+    CU_ASSERT( lseek( fd, 1000, SEEK_SET) >= 0 );
+        
+    memset( buffer1 + 1000, '=', 24);  // Write 24 bytes at position 1000 (beyond current size) 
+    CU_ASSERT( write( fd, buffer1 + 1000, 24) == 24);  // and write only bytes changed
+        
+    CU_ASSERT( lseek( fd, 0, SEEK_SET) >= 0 );
+
+    // Read entire file and compare. File must have been extended and filled with
+    // zeros in the range [512-1000]
+    CU_ASSERT( read( fd, buffer2, 1024) == 1024);
+
+    CU_ASSERT( memcmp( buffer1, buffer2, 1024) == 0 );
+      
+    CU_ASSERT( close(fd) >= 0);
+    CU_ASSERT( unlink( filename) == 0);
+}    
+
+// Test truncating files
+//
+void test_truncate( void)
+{
+    int fd, result;
+    char filename[ 32];
+    char buffer1[ 1024] = {0},
+         buffer2[ 1024];
+         
+    sprintf( filename, "testfile%d", rand());
+    
+    fd = open( filename, O_RDWR | O_CREAT, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP);
+    CU_ASSERT( fd >= 0 );
+    
+    memset( buffer1, '*', 1024);
+    CU_ASSERT( write( fd, buffer1, 1024) == 1024);
+    
+    // Read entire file. File must have be 1024 bytes
+    CU_ASSERT( lseek( fd, 0, SEEK_SET) >= 0 );
+    result = read( fd, buffer2, 1024);
+    CU_ASSERT( result == 1024);
+    
+    CU_ASSERT( ftruncate( fd, 512) == 0 );
+
+    // Read entire file. File must have been reduced to 512
+    CU_ASSERT( lseek( fd, 0, SEEK_SET) >= 0 );
+    result = read( fd, buffer2, 1024);  // Attempt or read beyond end
+    CU_ASSERT( result == 512);
+
+    CU_ASSERT( ftruncate( fd, 0) == 0 );
+
+    // Read entire file and compare. File must have been reduced to 512
+    CU_ASSERT( lseek( fd, 0, SEEK_SET) >= 0 );
+    result = read( fd, buffer2, 1024);  // Attempt or read beyond end
+    CU_ASSERT( result == 0);    // File must be empty
+
+    CU_ASSERT( close(fd) >= 0);
+    CU_ASSERT( unlink( filename) == 0);
+}    
+
+// Test rename
+//
+void test_rename( void)
+{
+    int fd;
+    char filename1[ 32],
+         filename2[ 32],
+         randomstr[ 32] = {0},
+         buffer[ 32];
+    
+    sprintf( filename1, "testfile%d", rand());
+    sprintf( filename2, "testfile%d", rand());
+    sprintf( randomstr, "randomic text %d", rand());
+    
+    // Create file 1
+    fd = open( filename1, O_RDWR | O_CREAT, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP);
+    CU_ASSERT( fd >= 0);
+    CU_ASSERT( write( fd, randomstr, 32) == 32);  // Write unique content
+    CU_ASSERT( close(fd) >= 0);
+    
+    // Create file 2
+    fd = open( filename2, O_RDWR | O_CREAT, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP);
+    CU_ASSERT( fd >= 0);
+    CU_ASSERT( close(fd) >= 0);
+    
+    CU_ASSERT( rename( filename1, filename2) == 0);  // Must replace existing file
+    
+    // Open without O_CREAT, must not exist
+    fd = open( filename1, O_RDWR, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP);
+    CU_ASSERT( fd < 0);
+ 
+     // Open without O_CREAT, must exist
+    fd = open( filename2, O_RDWR, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP);
+    CU_ASSERT( fd >= 0);
+    
+    // Read and verify that content written to file named filename1 is in filename2
+    CU_ASSERT( read( fd, buffer, 32) == 32);
+    CU_ASSERT( memcmp( buffer, randomstr, 32) == 0 );
+
+    CU_ASSERT( close(fd) >= 0);
+    CU_ASSERT( unlink( filename2) == 0);
+}
+
+// Test flags protection flags passed to open
+//
+void test_openflags( void)
+{
+    int fd;
+    char filename[ 32],
+         buffer[ 32] = {0};
+    
+    sprintf( filename, "testfile%d", rand());
+    
+    // Create file and open it write only
+    fd = open( filename, O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP);
+    CU_ASSERT( fd >= 0);
+    CU_ASSERT( write( fd, buffer, 32) == 32);  // Write some content
+
+    // Attempt to read the file, must fail
+    CU_ASSERT( read( fd, buffer, 32) < 0);
+    CU_ASSERT( errno == EBADF);
+
+    CU_ASSERT( close(fd) >= 0);
+    
+    // Reopen it read only
+    fd = open( filename, O_RDONLY, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP);
+    CU_ASSERT( fd >= 0);
+    
+    // Attempt to write some content, must fail
+    CU_ASSERT( write( fd, buffer, 32) < 0);  
+    CU_ASSERT( errno == EBADF);
+
+    CU_ASSERT( close(fd) >= 0);
+    CU_ASSERT( unlink( filename) == 0);
 }
 
 int main( int argc, char*argv[])
 {
-
+    CU_pSuite pSuite;
+    
     srand( time( NULL));
-
-    test_open();
-    test_offset();
+    
+    if ( CU_initialize_registry() != CUE_SUCCESS ) {
+        fprintf( stderr, "Failed to initialize CUnit\n");
+        exit( -1);
+    }
+    pSuite = CU_add_suite( "FUSE_for_Redis", NULL, NULL);
+    if ( pSuite == NULL) {
+        fprintf( stderr, "Failed to add suite error=%d\n", CU_get_error());
+        exit( -2);
+    }
+    
+    CU_ADD_TEST(pSuite, test_open);
+    CU_ADD_TEST(pSuite, test_offset);
+    CU_ADD_TEST(pSuite, test_extend);
+    CU_ADD_TEST(pSuite, test_truncate);
+    CU_ADD_TEST(pSuite, test_rename);
+    CU_ADD_TEST(pSuite, test_openflags);
+    
+    CU_basic_set_mode( CU_BRM_VERBOSE);
+    CU_basic_run_tests();
+    
+    CU_cleanup_registry();
 }
 
 
